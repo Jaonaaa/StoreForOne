3db62d3a068c94ed8580dff90c097ccc5154616a
Jaonaaa <jaonapaulferdinah@gmail.com>
2025-02-18 13:19:30 +0300

    fix: check si le document existe dans la gestion des cookies

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,6 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getCookie(name: string): string | undefined {
+  if (typeof document === "undefined") return undefined;
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop()?.split(";").shift();
